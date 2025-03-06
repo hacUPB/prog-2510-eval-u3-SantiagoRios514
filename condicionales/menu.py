1d4fea77e57416152d750cebe71ba1e18d0fce37
Santiago Rios <santiago.riosd@upb.edu.co>
2025-03-06 15:37:05 -0500

    ejercicio 5 y menu

---
+++ condicionales/menu.py
@@ -0,0 +1,12 @@
+# Operaciones Aritméticas
+
+print("S. suma\nR. Resta\nD. División\nM. Multiplicación")
+opcion = int(input("Elija una opción: "))
+
+if opcion == 'S':
+    print("Sumar")
+elif opcion == 'R':
+    print("Restar")
+elif opcion == 'D':
+    print("Dividir")
+    
